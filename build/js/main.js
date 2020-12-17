bd4435985e40f59e967bd7bce81678b0fcc442e6
mazax1ct <vova@giveandget.ru>
2020-12-17 19:03:16 +0300

    header + footer

---
+++ build/js/main.js
@@ -0,0 +1,52 @@
+//открытие главного меню
+$(document).on('click', '.js-menu-opener', function () {
+  $('body').addClass('overflow');
+  $('.header__dropdown').addClass('is-open');
+  document.addEventListener('click', closeMainMenu);
+  return false;
+});
+
+//закрытие главного меню
+$(document).on('click', '.js-menu-closer', function () {
+  $('body').removeClass('overflow');
+  $('.header__dropdown').removeClass('is-open');
+  return false;
+});
+
+function closeMainMenu(evt) {
+  if (!$('.main-menu__inner').is(evt.target) && $('.main-menu__inner').has(evt.target).length === 0) {
+    $('body').removeClass('overflow');
+    $('.header__dropdown').removeClass('is-open');
+    document.removeEventListener('click', closeMainMenu);
+	}
+}
+
+//открытие закрытие блока выбора языковой версии
+$(document).on('click', '.js-lang', function () {
+  $(this).toggleClass('is-active');
+  $(this).next('.lang__dropdown').toggleClass('is-open');
+  return false;
+});
+
+//второй уровень меню
+$(document).on('click', '.js-menu-lvl2', function () {
+  if($(this).hasClass('is-active')) {
+    $(this).removeClass('is-active');
+    $('.main-menu__dropdown').removeClass('is-open');
+    $('.main-menu__inner').removeClass('overflow');
+  } else {
+    $('.js-menu-lvl2').removeClass('is-active');
+    $('.main-menu__dropdown').removeClass('is-open');
+    $(this).addClass('is-active');
+    $('.main-menu__inner').addClass('overflow');
+    $(this).next('.main-menu__dropdown').addClass('is-open');
+  }
+  return false;
+});
+
+$(document).on('click', '.js-menu-lvl2-close', function () {
+  $('.js-menu-lvl2').removeClass('is-active');
+  $('.main-menu__inner').removeClass('overflow');
+  $('.main-menu__dropdown').removeClass('is-open');
+  return false;
+});
